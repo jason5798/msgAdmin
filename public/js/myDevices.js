4d70a3de972f5b13ab2c018e9e3df4a95a6918ce
jason5798 <jason_huang@gemteks.com>
2017-06-14 11:49:57 +0800

    [0.0.12]Add tabs and chart in device page
    1.tabs切換無法顯示chart
    2.將chart轉成image放到tabs下
    3.將chart隱藏
    4.在myDevice的showChart()實現

--- public/js/myDevices.js
+++ public/js/myDevices.js
@@ -15,9 +15,95 @@ var opt2={
 };
 var table = $('#table1').dataTable(opt2);
 
-var plot1;
+var plot1,plot2,plot3,plot4,plot5,plot6;
 var options1 ={
                 title: "PH",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options2 ={
+                title: "DO",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options2 ={
+                title: "DO",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options3 ={
+                title: "COND",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+
+var options4 ={
+                title: "Temperature",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+var options5 ={
+                title: "NTU",
+                axes:
+                {
+                    xaxis: {
+                        numberTicks: 24,
+                        renderer:$.jqplot.DateAxisRenderer,
+                        tickOptions:{formatString:'%H:%M'}
+                    },
+                    yaxis: {
+                        numberTicks: 10
+                    }
+                }
+            };
+
+var options6 ={
+                title: "Voltage",
                 axes:
                 {
                     xaxis: {
@@ -48,7 +134,7 @@ function wsConn() {
       console.log("from-node-red : id:"+msg.id);
       if(msg.id === 'init_table'){
           //Remove init button active
-          console.log("v : "+msg.v);
+          //console.log("v : "+msg.v);
 
           //Reload table data
           //console.log("v type:"+typeof(msg.v));
@@ -56,11 +142,11 @@ function wsConn() {
           table.fnClearTable();
           var data = JSON.parse(msg.v);
           //console.log("addData type : "+ typeof(data)+" : "+data);
-          showChart(data);
           if(data){
               table.fnAddData(data);
           }
           waitingDialog.hide();
+          showChart(data);
       }
     }
   }
@@ -118,18 +204,34 @@ function back(){
 }
 
 function showChart(data){
-    var tempList = [],humList=[];
+    var tempList = [],doList=[];
+    var condList = [],ntuList=[];
+    var phList = [],volList=[];
     //alert('data.length : '+data.length);
     for(i=0;i<data.length;i++){
         //alert('i : '+i);
         if(i > (data.length-1) ){
             break;
         }else{
-            tempList.push([new Date(data[i][1]).getTime(),data[i][3] ]);
-            if(i<2){
-              alert('tempList :'+JSON.stringify(tempList));
-            }
+
+            phList.push([new Date(data[i][1]).getTime(),data[i][3] ]);
+            doList.push([new Date(data[i][1]).getTime(),data[i][4] ]);
+            condList.push([new Date(data[i][1]).getTime(),data[i][5] ]);
+            tempList.push([new Date(data[i][1]).getTime(),data[i][6] ]);
+            ntuList.push([new Date(data[i][1]).getTime(),data[i][7] ]);
+            volList.push([new Date(data[i][1]).getTime(),data[i][8] ]);
         }
+
+        /*if(i<2){
+          alert('data[i] : '+JSON.stringify(data[i]) );
+        alert('data[i][8] : '+data[i][3]);
+        alert('data[i][8] : '+data[i][4]);
+        alert('data[i][8] : '+data[i][5]);
+        alert('data[i][8] : '+data[i][6]);
+        alert('data[i][8] : '+data[i][7]);
+        alert('data[i][8] : '+data[i][8]);
+        }*/
+
     }
 
 
@@ -155,50 +257,86 @@ function showChart(data){
 
     if(plot1){
         plot1.destroy();
-    }
-    /*if(plot2){
         plot2.destroy();
-    }*/
+        plot3.destroy();
+        plot4.destroy();
+        plot5.destroy();
+        plot6.destroy();
 
+    }
 
     options1.axes.xaxis.numberTicks = x_number;
-    //options2.axes.xaxis.numberTicks = x_number;
+    options2.axes.xaxis.numberTicks = x_number;
+    options3.axes.xaxis.numberTicks = x_number;
+    options4.axes.xaxis.numberTicks = x_number;
+    options5.axes.xaxis.numberTicks = x_number;
+    options6.axes.xaxis.numberTicks = x_number;
     options1.axes.xaxis.tickOptions.formatString = formatStr;
-    //options2.axes.xaxis.tickOptions.formatString = formatStr;
-    plot1 = $.jqplot ('chartTmp', [tempList],options1);
-    //plot2 = $.jqplot ('chartHum', [humList],options2);
+    options2.axes.xaxis.tickOptions.formatString = formatStr;
+    options3.axes.xaxis.tickOptions.formatString = formatStr;
+    options4.axes.xaxis.tickOptions.formatString = formatStr;
+    options5.axes.xaxis.tickOptions.formatString = formatStr;
+    options6.axes.xaxis.tickOptions.formatString = formatStr;
+    //Jason add for chart to image on 2017.06.12
+
+    plot1 = $.jqplot ('chartPH', [phList],options1);
+    plot2 = $.jqplot ('chartDO', [doList],options2);
+    plot3 = $.jqplot ('chartCOND', [condList],options3);
+    plot4 = $.jqplot ('chartTmp', [tempList],options4);
+    plot5 = $.jqplot ('chartNTU', [ntuList],options5);
+    plot6 = $.jqplot ('chartVol', [volList],options6);
+    var imgPHData = $('#chartPH').jqplotToImageStr({});
+    var imgPHElem = $('<img/>').attr('src',imgPHData);
+    $('#imgChart1').append(imgPHElem);
+    $("#chartPH").hide();
+    var imgDOData = $('#chartDO').jqplotToImageStr({});
+    var imgDOElem = $('<img/>').attr('src',imgDOData);
+    $('#imgChart2').append(imgDOElem);
+    $("#chartDO").hide();
+    var imgCONDData = $('#chartCOND').jqplotToImageStr({});
+    var imgCONDElem = $('<img/>').attr('src',imgCONDData);
+    $('#imgChart3').append(imgCONDElem);
+    $("#chartCOND").hide();
+    var imgTmpData = $('#chartTmp').jqplotToImageStr({});
+    var imgTmpElem = $('<img/>').attr('src',imgTmpData);
+    $('#imgChart4').append(imgTmpElem);
+    $("#chartTmp").hide();
+    var imgNTUData = $('#chartNTU').jqplotToImageStr({});
+    var imgNTUElem = $('<img/>').attr('src',imgNTUData);
+    $('#imgChart5').append(imgNTUElem);
+    $("#chartNTU").hide();
+    var imgVolData = $('#chartVol').jqplotToImageStr({});
+    var imgVolElem = $('<img/>').attr('src',imgVolData);
+    $('#imgChart6').append(imgVolElem);
+    $("#chartVol").hide();
 }
 
+function changeChartDisplay(){
+  var obj = document.getElementById("chartBtn");
+  alert(obj.value);
+  if(obj.value === 'Show Chart'){
+    obj.value = 'Hide Chart';
+   $("#chartBlock").hide();
+  }else{
+    obj.value = 'Show Chart';
+    $("#chartBlock").show();
+  }
+}
 
 $(document).ready(function(){
     showDialog();
 
 
-    /*table.$('tr').click(function() {
-        var row=table.fnGetData(this);
-        toSecondTable(row[1]);
-
-    });*/
-
-
-    //$("#table1").dataTable(opt); //中文化
-
-
-    /*table.$('tr').click(function() {
-        var row=table.fnGetData(this);
-        toSecondTable(row[1]);
-
-    });
-
-     $("#table1").on({
-          mouseenter: function(){
-           //stuff to do on mouse enter
-
-           $(this).css({'color':'blue'});
-           },
-           mouseleave: function () {
-           //stuff to do on mouse leave
-           $(this).css({'color':'black'});
-      }},'tr');*/
+  $('#chartBlock').tabs({
+    onSelect:function(title){
+        if(title==='DO'){
+              alert(title);
+             $('#my_tabs').tabs('load', 0);
+        }
+      },
+      onLoad:function(panel){
+        alert(panel.panel('options').title+' is loaded')
+      }
+    })
 
 });
